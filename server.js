e68bc646976f14f333fee3c5f82d1e651f9873e6
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2023-01-29 23:33:54 +0530

    frontend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const connectDB = require('./config/db');
+const path =require('path')
  
 
 const app=express();
@@ -13,7 +14,7 @@ app.use(express.json({extended:false}))
  
 
 
-app.get('/',(req,res)=>res.send('API Running'));
+
 
 //routes 
 
@@ -24,9 +25,16 @@ app.use('/api/hospitals',require('./routes/api/hospitals'))
 app.use('/api/post',require('./routes/api/post'))
 app.use('/api/request',require('./routes/api/request'))
  
-
-
-
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 const PORT =process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server Started on ${PORT}`))
